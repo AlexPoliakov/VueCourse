e79b06190374bd7121e2bd9ac8e9cc1f995b5a81
Alex Poliakov <alpoliakov@ukr.net>
2017-09-29 16:43:07 +0300

    Implementation of the registration form on the Vue without components

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
-Vue.component('list', {
-    template: `<ul><li v-for="car in listCars">{{ car.car }}</li></ul>`,
-    data () {
-        return {
-            listCars: [
-                {car: "Audi"},
-                {car: "Opel"},
-                {car: "BMW"},
-                {car: "Tesla"}
-            ]
-        }
-    }
-});
-
-
 const vm = new Vue({
-    el: '#app',
-    data: {
-        text: "Hello world!",
+  el: '#box',
+  data() {
+    return {
+      text: 'Registration Form',
+      listUsers: [],
+      name: '',
+      lastName: '',
+      email: '',
+      message: 'Add your photo',
+      messageChange: 'Change user data',
+      messageDelete: 'Delete user data',
+      show: false,
+      change: {},
+    };
+  },
+  methods: {
+    clearForm() {
+      this.name = '';
+      this.lastName = '';
+      this.email = '';
+      this.show = false;
     },
-    methods: {
-        reverseMessage() {
-            this.text = [...this.text].reverse().join('')
-        }
+    addUser() {
+      if (this.name === '' || this.lastName === '' || this.email === '') {
+        alert('Fill in all fields of the form');
+        return;
+      }
+      this.listUsers.push({
+        name: this.name,
+        lastName: this.lastName,
+        email: this.email,
+      });
+      this.clearForm();
+    },
+    deleteUser(index) {
+      this.listUsers.splice(index, 1);
+      this.clearForm();
+    },
+    changeData(key) {
+      this.show = true;
+      this.name = this.listUsers[key].name;
+      this.lastName = this.listUsers[key].lastName;
+      this.email = this.listUsers[key].email;
+      this.change = this.listUsers[key];
+    },
+    saveChange() {
+      this.change.name = this.name;
+      this.change.lastName = this.lastName;
+      this.change.email = this.email;
+      this.clearForm();
     }
+  }
 });
